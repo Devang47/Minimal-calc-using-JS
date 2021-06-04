d71267d02ec07a6f7bef9b46be7dfc23f1551980
devang <devangsaklani@gmail.com>
2021-06-04 20:04:09 +0530

    v2

--- JS/index.js
+++ JS/index.js
@@ -2,19 +2,26 @@ let input = "";
 let output = document.querySelector("#answerbox");
 
 window.onload = () => {
-    output.value = ""
+  output.value = "";
+};
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.toggle(localStorage.getItem("theme"));
 }
 
 const color = () => {
-    output.style.color = "#fff";
-}
+  // output.style.color = "#fff";
+};
 
+//input reciever
 const inputreciever = (e) => {
   input = output.value;
-  output.style.color = "#fff";
+  // output.style.color = "#fff";
   input += e;
   output.value = input;
 };
+
+//equals btn
 const findans = () => {
   input = output.value;
   try {
@@ -27,17 +34,21 @@ const findans = () => {
   output.value = input;
   input = "" + input;
 };
+
+// reset btn
 const reset = () => {
   input = "";
   output.value = input;
 };
 
+//delete btn
 const deleteLast = () => {
   input = output.value;
   input = input.slice(0, -1);
   output.value = input;
 };
 
+//copy btn
 var text = document.getElementById("answerbox");
 var btn = document.getElementById("copyText");
 
@@ -46,3 +57,14 @@ btn.onclick = function () {
   text.select();
   document.execCommand("copy");
 };
+
+const themeSwitch = document.querySelector(".inner")
+const themeSwitcher = document.getElementById("theme");
+themeSwitcher.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+  themeSwitch.classList.toggle("theme2")
+  // themeSwitch.style.left = "20px"
+  localStorage.setItem("theme", document.body.classList);
+});
+
+
